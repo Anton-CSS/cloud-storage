16e22c1501d37b0d9569c9753bfaf4ce91d3e31e
Anton Rokunets <9251580262@mail.ru>
2021-10-29 23:23:15 +0300

    preparing project

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  parser: "@babel/eslint-parser",
+  extends: ["airbnb-base", "prettier", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-plusplus": 0,
+    "no-alert": "off",
+    "no-console": "off",
+    "consistent-return": "off",
+    "no-param-reassign": "off",
+  },
+  plugins: ["jest"],
+};
