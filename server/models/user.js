60ecee464f846474d50fb6dcc991a37df5c54980
Anton Rokunets <9251580262@mail.ru>
2021-11-03 19:57:58 +0300

    add router login

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,12 @@
-import ObjectId, { Schema, model } from "mongoose";
+const { Schema, model, ObjectId } = require("mongoose");
 
 const User = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   diskSpace: { type: Number, default: 1024 ** 3 * 10 },
-  usedSpace: { type: Number, required: 0 },
+  usedSpace: { type: Number, default: 0 },
   avatar: { type: String },
   files: [{ type: ObjectId, ref: "File" }],
 });
 
-export default model("User", User);
+module.exports = model("User", User);
